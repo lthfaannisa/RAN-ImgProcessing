d912dda970e856ed9103c4cea229f05b016b8de0
raishabirah <raihanshabirah20@gmail.com>
2023-09-21 23:05:25 +0700

    feat: add blur image

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from werkzeug.utils import secure_filename
 import os
 import matplotlib.pyplot as plt
 import cv2
+from histoEqual import calculate_histogram, save_histogram_image, equalize_image
+from blur import apply_blur
 
 app = Flask(__name__)
 
@@ -18,64 +20,40 @@ def upload_file():
         file.save(os.path.join(app.config['UPLOAD'], filename))
         img_path = os.path.join(app.config['UPLOAD'], filename)
 
-        # Membaca gambar dengan OpenCV
-        img = cv2.imread(img_path)
+        if 'equalize' in request.form:  # Tombol "Histogram Equalization" ditekan
+            # Menghitung histogram
+            hist_r, hist_g, hist_b = calculate_histogram(img_path)
 
-        # Menghitung histogram untuk masing-masing saluran (R, G, B)
-        hist_r = cv2.calcHist([img], [0], None, [256], [0, 256])
-        hist_g = cv2.calcHist([img], [1], None, [256], [0, 256])
-        hist_b = cv2.calcHist([img], [2], None, [256], [0, 256])
+            # Simpan histogram sebagai gambar PNG
+            hist_image_path = os.path.join(app.config['UPLOAD'], 'histogram.png')
+            save_histogram_image(hist_r, hist_g, hist_b, hist_image_path)
 
-        # Normalisasi histogram
-        hist_r /= hist_r.sum()
-        hist_g /= hist_g.sum()
-        hist_b /= hist_b.sum()
+            # Equalisasi histogram
+            img_equalized = equalize_image(img_path)
 
-        # Simpan histogram sebagai gambar PNG
-        hist_image_path = os.path.join(app.config['UPLOAD'], 'histogram.png')
-        plt.figure()
-        plt.title("RGB Histogram")
-        plt.xlabel("Value")
-        plt.ylabel("Frequency")
-        plt.plot(hist_r, color='red', label='Red')
-        plt.plot(hist_g, color='green', label='Green')
-        plt.plot(hist_b, color='blue', label='Blue')
-        plt.legend()
-        plt.savefig(hist_image_path)
+            # Menyimpan gambar hasil equalisasi ke folder "static/uploads"
+            equalized_image_path = os.path.join('static', 'uploads', 'img-equalized.jpg')
+            cv2.imwrite(equalized_image_path, img_equalized)
 
-        # Hasil equalisasi
-        img_equalized = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)  # Ubah ke ruang warna YCrCb
-        img_equalized[:, :, 0] = cv2.equalizeHist(img_equalized[:, :, 0])  # Equalisasi komponen Y (luminance)
-        img_equalized = cv2.cvtColor(img_equalized, cv2.COLOR_YCrCb2BGR)  # Kembalikan ke ruang warna BGR
+            # Menghitung histogram untuk gambar yang sudah diequalisasi
+            hist_equalized_r, hist_equalized_g, hist_equalized_b = calculate_histogram(equalized_image_path)
 
-        # Menyimpan gambar hasil equalisasi ke folder "static/uploads"
-        equalized_image_path = os.path.join('static', 'uploads', 'img-equalized.jpg')
-        cv2.imwrite(equalized_image_path, img_equalized)
+            # Simpan histogram hasil equalisasi sebagai gambar PNG
+            hist_equalized_image_path = os.path.join(app.config['UPLOAD'], 'histogram_equalized.png')
+            save_histogram_image(hist_equalized_r, hist_equalized_g, hist_equalized_b, hist_equalized_image_path)
 
-        # Menghitung histogram untuk gambar yang sudah diequalisasi
-        hist_equalized_r = cv2.calcHist([img_equalized], [0], None, [256], [0, 256])
-        hist_equalized_g = cv2.calcHist([img_equalized], [1], None, [256], [0, 256])
-        hist_equalized_b = cv2.calcHist([img_equalized], [2], None, [256], [0, 256])
+            return render_template('index.html', img=img_path, img2=equalized_image_path, histogram=hist_image_path, histogram2=hist_equalized_image_path)
 
-        # Normalisasi histogram
-        hist_equalized_r /= hist_equalized_r.sum()
-        hist_equalized_g /= hist_equalized_g.sum()
-        hist_equalized_b /= hist_equalized_b.sum()
+        elif 'blur' in request.form:  # Tombol "Blur" ditekan
+            # Efek blur
+            img_blurred = apply_blur(img_path)  # Menggunakan pengaturan default
 
-        # Simpan histogram hasil equalisasi sebagai gambar PNG        
-        hist_equalized_image_path = os.path.join(app.config['UPLOAD'], 'histogram_equalized.png')
-        plt.figure()
-        plt.title("RGB Histogram (Equalized)")
-        plt.xlabel("Value")
-        plt.ylabel("Frequency")
-        plt.plot(hist_equalized_r, color='red', label='Red')
-        plt.plot(hist_equalized_g, color='green', label='Green')
-        plt.plot(hist_equalized_b, color='blue', label='Blue')
-        plt.legend()
-        plt.savefig(hist_equalized_image_path)
+            # Menyimpan gambar hasil blur ke folder "static/uploads"
+            blurred_image_path = os.path.join('static', 'uploads', 'img-blurred.jpg')
+            cv2.imwrite(blurred_image_path, img_blurred)
 
-        return render_template('index.html', img=img_path, img2=equalized_image_path, histogram=hist_image_path, histogram2=hist_equalized_image_path)
-    
+            return render_template('index.html', img=img_path, img2=blurred_image_path)
+
     return render_template('index.html')
 
 
