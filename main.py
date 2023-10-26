8e00b50793a7e3d087694dcdbccc655a5e28d1fe
raishabirah <raihanshabirah20@gmail.com>
2023-10-26 15:28:30 +0700

    feat: cleaning code

--- main.py
+++ main.py
@@ -14,9 +14,7 @@ from closingMorphology import apply_closing
 from dilationMorphology import apply_dilation
 
 app = Flask(__name__)
-
 upload_folder = os.path.join('static', 'uploads')
-
 app.config['UPLOAD'] = upload_folder
 
 @app.route('/', methods=['GET', 'POST'])
@@ -27,104 +25,91 @@ def upload_file():
         file.save(os.path.join(app.config['UPLOAD'], filename))
         img_path = os.path.join(app.config['UPLOAD'], filename)
 
-        if 'equalize' in request.form:  # Tombol "Histogram Equalization" ditekan
+        if 'equalize' in request.form:
             # Menghitung histogram
             hist_r, hist_g, hist_b = calculate_histogram(img_path)
-
-            # Simpan histogram sebagai gambar PNG
             hist_image_path = os.path.join(app.config['UPLOAD'], 'histogram.png')
             save_histogram_image(hist_r, hist_g, hist_b, hist_image_path)
 
             # Equalisasi histogram
             img_equalized = equalize_image(img_path)
-
-            # Menyimpan gambar hasil equalisasi ke folder "static/uploads"
             equalized_image_path = os.path.join('static', 'uploads', 'img-equalized.jpg')
             cv2.imwrite(equalized_image_path, img_equalized)
 
             # Menghitung histogram untuk gambar yang sudah diequalisasi
             hist_equalized_r, hist_equalized_g, hist_equalized_b = calculate_histogram(equalized_image_path)
-
-            # Simpan histogram hasil equalisasi sebagai gambar PNG
             hist_equalized_image_path = os.path.join(app.config['UPLOAD'], 'histogram_equalized.png')
             save_histogram_image(hist_equalized_r, hist_equalized_g, hist_equalized_b, hist_equalized_image_path)
 
             return render_template('index.html', img=img_path, img2=equalized_image_path, histogram=hist_image_path, histogram2=hist_equalized_image_path)
 
-        elif 'blur_face' in request.form:  # Tombol "Blur Face" ditekan
+        elif 'blur_face' in request.form:
+            # Inisialisasi cascade classifier
             face_cascade = cv2.CascadeClassifier('static\haarcascade_frontalface_default.xml')
             
             # Efek blur wajah
-            img_blurred = apply_face_blur(img_path, face_cascade)  # Menggunakan cascade classifier yang telah diinisialisasi
-
-            # Menyimpan gambar hasil blur wajah ke folder "static/uploads"
+            img_blurred = apply_face_blur(img_path, face_cascade)
             blurred_image_path = os.path.join('static', 'uploads', 'img-blurred.jpg')
             cv2.imwrite(blurred_image_path, img_blurred)
 
             return render_template('index.html', img=img_path, img2=blurred_image_path)
         
-        elif 'detect_edges' in request.form:  # Tombol "Edge Detection" ditekan
+        elif 'detect_edges' in request.form:
+            # Deteksi tepi gambar
             edges = detect_edges(img_path)
-
-            # Menyimpan gambar hasil deteksi tepi ke folder "static/uploads"
             edges_image_path = os.path.join('static', 'uploads', 'img-edges.jpg')
             cv2.imwrite(edges_image_path, edges)
 
             return render_template('index.html', img=img_path, img2=edges_image_path)
         
-        elif 'segment_image' in request.form:  # Tombol "Segmentasi Citra" ditekan
-            num_clusters = 3  # Ganti jumlah klaster sesuai kebutuhan
+        elif 'segment_image' in request.form:
+            # Menentukan jumlah cluster
+            num_clusters = 3
+            
+            # Segmentasi Gambar
             segmented_image = cluster_segmentation(img_path, num_clusters)
-
-            # Menyimpan gambar hasil segmentasi ke folder "static/uploads"
             segmented_image_path = os.path.join('static', 'uploads', 'img-segmented.jpg')
             cv2.imwrite(segmented_image_path, cv2.cvtColor(segmented_image, cv2.COLOR_RGB2BGR))
 
             return render_template('index.html', img=img_path, img2=segmented_image_path)
         
-        elif 'add_moustache' in request.form:  # Tombol "Tambah Kumis" ditekan
-            img_with_moustache = add_moustache_to_face(img_path)  # Menambahkan kumis
-
-            # Menyimpan gambar hasil tambah kumis ke folder "static/uploads"
+        elif 'add_moustache' in request.form:
+            # Menambahkan kumis
+            img_with_moustache = add_moustache_to_face(img_path)
             img_with_moustache_path = os.path.join('static', 'uploads', 'img-with-moustache.jpg')
             cv2.imwrite(img_with_moustache_path, img_with_moustache)
 
             return render_template('index.html', img=img_path, img2=img_with_moustache_path)
         
-        elif 'reduce_noise' in request.form:  # Tombol "Reduce Noise" ditekan
+        elif 'reduce_noise' in request.form:
             # Mengurangi noise pada gambar
             img_smoothed = reduce_noise(img_path)
-
-            # Menyimpan gambar hasil pengurangan noise ke folder "static/uploads"
             smoothed_image_path = os.path.join('static', 'uploads', 'img-smoothed.jpg')
             cv2.imwrite(smoothed_image_path, img_smoothed)
 
             return render_template('index.html', img=img_path, img2=smoothed_image_path)
         
-        elif 'apply_opening' in request.form:  # Tombol "Opening Morphology" ditekan
-            # Menjalankan operasi Opening Morphology
-            opened_image = apply_opening(img_path)
+        elif 'apply_dilation' in request.form:
+            # Menjalankan operasi Dilasi
+            img_dilation = apply_dilation(img_path)
+            dilation_image_path = os.path.join('static', 'uploads', 'img-closed.jpg')
+            cv2.imwrite(dilation_image_path, img_dilation)
 
-            # Menyimpan gambar hasil Opening Morphology ke folder "static/uploads"
+            return render_template('index.html', img=img_path, img2=dilation_image_path)
+        
+        elif 'apply_opening' in request.form:
+            # Menjalankan operasi Opening
+            opened_image = apply_opening(img_path)
             opened_image_path = os.path.join('static', 'uploads', 'img-opened.jpg')
             cv2.imwrite(opened_image_path, opened_image)
 
             return render_template('index.html', img=img_path, img2=opened_image_path)
-        elif 'apply_closing' in request.form:  # Tombol "Closing" ditekan
+        
+        elif 'apply_closing' in request.form:
+            # Menjalankan operasi Closing
             img_closed = apply_closing(img_path)
-
-            # Menyimpan gambar hasil operasi closing ke folder "static/uploads"
             closed_image_path = os.path.join('static', 'uploads', 'img-closed.jpg')
             cv2.imwrite(closed_image_path, img_closed)
-
-            return render_template('index.html', img=img_path, img2=closed_image_path)
-        
-        elif 'apply_dilation' in request.form:  # Tombol "Closing" ditekan
-            img_dilation = apply_dilation(img_path)
-
-            # Menyimpan gambar hasil operasi closing ke folder "static/uploads"
-            dilation_image_path = os.path.join('static', 'uploads', 'img-closed.jpg')
-            cv2.imwrite(dilation_image_path, img_dilation)
 
             return render_template('index.html', img=img_path, img2=closed_image_path)
         
