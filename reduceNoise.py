54d9118aaf4389e5a68bbdbb728c3c9e2086f32b
Annisa Lathifa <annisalathifa1234@gmail.com>
2023-10-26 10:47:17 +0700

    docs: modified some files

--- reduceNoise.py
+++ reduceNoise.py
@@ -4,7 +4,7 @@ def reduce_noise(img_path):
     # Baca gambar
     img = cv2.imread(img_path)
 
-    # Terapkan filter Gaussian untuk mengurangi noise
-    img_smoothed = cv2.GaussianBlur(img, (5, 5), 0)
+    # Terapkan filter bilateral untuk mengurangi noise
+    img_smoothed = cv2.bilateralFilter(img, 5, 100, 100)
 
     return img_smoothed
